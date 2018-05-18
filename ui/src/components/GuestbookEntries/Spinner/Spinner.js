1158fcc5d28844b193d29826ed44f3515ac67b2c
Tyler Wells <tyler.wellss@gmail.com>
2018-05-18 18:33:59 +0800

    add loading spinner component

---
+++ ui/src/components/GuestbookEntries/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css'
+
+const spinner = () => {
+  return (
+    <div className={'loader'}>
+      Loading ...
+    </div>
+  )
+}
+
+export default spinner;
